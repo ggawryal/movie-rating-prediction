2eca347f26df3e7fb480f3e2b26ef2755c6cd33c
ggawryal <ggawryal@gmail.com>
2021-01-19 14:17:26 +0100

    abs and huber loss for gradient boosting

--- project.py
+++ project.py
@@ -2,11 +2,11 @@ import numpy as np
 import math
 from sklearn.metrics import mean_squared_error, r2_score
 import sklearn.preprocessing 
-
+from sklearn.ensemble import GradientBoostingRegressor
 import data_preparation
 
-data_train, data_test = data_preparation.get_data(0.7,save_to_file=True)
-#data_train, data_test = data_preparation.get_data_from_tmp()
+#data_train, data_test = data_preparation.get_data(0.7,save_to_file=True)
+data_train, data_test = data_preparation.get_data_from_tmp()
 np.random.shuffle(data_test)
 #data_val,data_test = data_test[:len(data_test)//2], data_test[len(data_test)//2:]
 
@@ -52,13 +52,21 @@ class Decision_Tree_Regression:
             self.left = left
             self.right = right
     
-    def __init__(self, max_depth=40):
+    def __init__(self, max_depth=40,leaf_prediction_function = "mean"):
         self.max_depth = max_depth
+        if leaf_prediction_function == "mean":
+            self.leaf_prediction_function = lambda Xy: np.mean(Xy[:,-1])
+        elif leaf_prediction_function == "median":
+            self.leaf_prediction_function = lambda Xy: np.median(Xy[:,-1])
+        elif callable(leaf_prediction_function):
+            self.leaf_prediction_function = leaf_prediction_function
+        else:
+            raise RuntimeError("unsupported leaf prediction function")
 
-    def find_split_point(self,Xy):
+    def find_split_point(self,Xy,ignored_last_features_n):
         n,m = Xy.shape[1]-1, Xy.shape[0]
         best = (1e18,None,None)
-        for j in range(n):
+        for j in range(n-ignored_last_features_n):
             Xs = Xy[np.argsort(Xy[:,j])]
             
             sumL, sumR = 0, Xy[:,-1].sum()
@@ -78,29 +86,30 @@ class Decision_Tree_Regression:
 
         return (best[1], best[2])
     
-    def fit_recursive(self, Xy, node, depth):
+    def fit_recursive(self, Xy, node, depth, ignored_last_features_n):
         leaf = False
         if len(Xy) <= 5 or depth == self.max_depth:
             leaf = True
         else:
-            j,s = self.find_split_point(Xy)
+            j,s = self.find_split_point(Xy,ignored_last_features_n)
             if j is None:
                 leaf = True
             else:
                 XyL, XyR = Xy[Xy[:,j] <= s, :], Xy[Xy[:,j] > s, :]
                 node.set_params(j,s,  Decision_Tree_Regression.TreeNode(),  Decision_Tree_Regression.TreeNode())
                 #print('split by '+feature_names[j]+' <= ',s,len(XyL),len(XyR))
-                self.fit_recursive(XyL,node.left,depth+1)
-                self.fit_recursive(XyR,node.right,depth+1)
+                self.fit_recursive(XyL,node.left,depth+1,ignored_last_features_n)
+                self.fit_recursive(XyR,node.right,depth+1,ignored_last_features_n)
         if leaf:
             assert(len(Xy) > 0)
             node.set_params(None,None,None,None)
-            node.c = Xy[:,-1].sum()/len(Xy)
+            #node.c = np.mean(Xy[:,-1])
+            node.c = self.leaf_prediction_function(Xy)
             return 
 
-    def fit(self,X,y):
+    def fit(self,X,y, ignored_last_features_n = 0):
         self.root = Decision_Tree_Regression.TreeNode()
-        self.fit_recursive(np.append(X,y.reshape(-1,1),axis=1), self.root, 0)
+        self.fit_recursive(np.append(X,y.reshape(-1,1),axis=1), self.root, 0,ignored_last_features_n)
 
     def predict_single(self,x):
         if(x.ndim != 1):
@@ -117,7 +126,7 @@ class Decision_Tree_Regression:
         return np.array([self.predict_single(x) for x in X])
 
 class Gradient_Boosting_Regression:
-    def __init__(self, iters=100, max_tree_depth=4, eta=0.1, sample_fraction=1.0):
+    def __init__(self, iters=100, max_tree_depth=4, eta=0.1, sample_fraction=1.0, loss = "L2", huber_alpha_quantile=0.6):
         """
         Parameters
         ----------
@@ -130,33 +139,60 @@ class Gradient_Boosting_Regression:
         sample_fraction 
             fraction of random sample of rows used in each iteration, 
             1.0 means using whole set of rows, smaller values can be used for stochastic gradient boosting
+        loss
+            loss function for gradient boosting, could be either L2 (squared error, LS_Boost), L1 (abs error, LAD_TreeBost) or huber(M_TreeBost)
+        huber_alpha_quantile
+            quantile of how many values are considered as outliers in huber loss function, in each iteration huber delta parameter is calculated based on this
         """
         self.iters = iters
         self.max_tree_depth = max_tree_depth
         self.eta = eta
         self.sample_fraction = sample_fraction
+        self.huber_alpha_quantile = huber_alpha_quantile
+        self.loss = loss
+        if loss == "L2":
+            self.gradient = lambda x,y,: x-y
+        elif loss == "L1":
+            self.gradient = lambda x,y: np.sign(x-y)
+        elif loss == "huber":
+            self.gradient = lambda x,y,delta: x-y if np.abs(x-y) <= delta else delta*np.sign(x-y)
+        else:
+            raise("unsupported loss function")
 
     def update_function(self,f,tree):
         return lambda x: f(x) + self.eta*tree.predict_single(x)
 
     def fit(self,X,y):
         n,m = X.shape[1], X.shape[0]
-        
-        mean_y = sum(y)/len(y)
-        fs = [lambda _ : mean_y] + [None] * self.iters
+        starting_value_function = np.mean if self.loss == "lse" else np.median
+
+        fs = [lambda _ : starting_value_function(y)] + [None] * self.iters
 
         for t in range(self.iters):
             random_indexes = np.random.choice(X.shape[0], max(1,int(m*self.sample_fraction)), replace=False)
             X2,y2 = X[random_indexes], y[random_indexes]
-            g = np.array([y2[i] - fs[t](X2[i]) for i in range(len(y2))])
 
-            tree = Decision_Tree_Regression(self.max_tree_depth)
-            tree.fit(X2,g)
+            if self.loss == "huber":
+                r = y2 - np.apply_along_axis(fs[t],1,X2)
+                delta = np.quantile(r,self.huber_alpha_quantile)
+                g = np.array([self.gradient(y2[i], fs[t](X2[i]),delta) for i in range(len(y2))])
+                X2 = np.append(X2, r.reshape(-1,1),axis=1)
+
+                def leaf_prediction_function(Xy2):
+                    r_tilde = np.median(Xy2[:,-2])
+                    return r_tilde + np.mean(np.sign(Xy2[:,-2]-r_tilde)*np.minimum(np.abs(Xy2[:,-2] - r_tilde),delta))    
+
+                tree = Decision_Tree_Regression(self.max_tree_depth,leaf_prediction_function)
+                tree.fit(X2,g, ignored_last_features_n=1)
+
+            else:
+                g = np.array([self.gradient(y2[i], fs[t](X2[i])) for i in range(len(y2))])
+                tree = Decision_Tree_Regression(self.max_tree_depth, "mean" if self.loss == "L2" else "median")
+                tree.fit(X2,g)
+
             fs[t+1] = self.update_function(fs[t],tree)
-            
             #y_pred = np.array([fs[t+1](x) for x in X])
             #print("t =",t,"mse =",mean_squared_error(y_pred,y))
-
         self.h = fs[-1]
 
     def predict_single(self,x):
@@ -166,10 +202,11 @@ class Gradient_Boosting_Regression:
         return np.array([self.predict_single(x) for x in X])
 
 class Random_Forest_Regression:
-    def __init__(self, n_trees, max_depth, bootstrap=True, max_features="all"):
+    def __init__(self, n_trees, max_depth, bootstrap=True, max_features="all", alpha_prunning=0):
         self.n_trees = n_trees
         self.max_depth = max_depth
         self.bootstrap = bootstrap
+        self.alpha_prunning = alpha_prunning
 
         if isinstance(max_features,int):
             self.n_features = lambda _ : max_features
@@ -201,11 +238,12 @@ class Random_Forest_Regression:
         return np.array([self.predict_single(x) for x in X])
 
 #regr = LinearRegression(C=1)
-regr = Decision_Tree_Regression(max_depth=6)
-#regr = Gradient_Boosting_Regression(iters=30, max_tree_depth=4,sample_fraction=0.6)
-#regr = Random_Forest_Regression(n_trees = 20,max_depth=7,bootstrap=True,max_features="sqrt")
+#regr = Decision_Tree_Regression(max_depth=6)
+regr = Gradient_Boosting_Regression(iters=30, max_tree_depth=4, sample_fraction=0.6, loss="huber",huber_alpha_quantile=0.8)
+#regr = Gradient_Boosting_Regression(iters=30, max_tree_depth=4, sample_fraction=0.6, loss="L1",huber_alpha_quantile=0.1)
+#regr = Random_Forest_Regression(n_trees = 50,max_depth=4,bootstrap=True,max_features="sqrt")
+#regr.fit(d_X_train, d_y_train)
 regr.fit(d_X_train, d_y_train)
-
 d_y_pred = regr.predict(d_X_test)
 d_y_pred_tr = regr.predict(d_X_train)
 
@@ -213,3 +251,4 @@ print('MSE on train: %.2f' % mean_squared_error(d_y_train, d_y_pred_tr))
 print('MSE on test: %.2f' % mean_squared_error(d_y_test, d_y_pred))
 print('R2: %.2f'     % r2_score(d_y_test, d_y_pred))
 
+g = GradientBoostingRegressor(loss="huber")
